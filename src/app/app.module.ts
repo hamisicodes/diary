85acd0f9b469f6810ecc5b1d1620fb583a9a9caa
hamisicodes <newhamisi@gmail.com>
2020-03-30 12:56:01 +0300

    implement directive with line-through for testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { DiaryComponent } from './diary/diary.component';
 import { DiaryDetailComponent } from './diary-detail/diary-detail.component';
 import { DiaryFormComponent } from './diary-form/diary-form.component';
 import { FormsModule } from '@angular/forms';
-import { StrikethroughDirective } from './strikethrough.directive';
+
 import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
@@ -16,7 +16,7 @@ import { HighlightDirective } from './highlight.directive';
     DiaryComponent,
     DiaryDetailComponent,
     DiaryFormComponent,
-    StrikethroughDirective,
+    
     HighlightDirective
   ],
   imports: [
